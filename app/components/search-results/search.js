376f1455c598598be241042db5eb0904c57c0ce2
Elias Carlston <elias@eliascarlston.com>
2015-03-04 21:35:01 -0600

    Search URL hooked up

--- app/components/search-results/search.js
+++ app/components/search-results/search.js
@@ -3,12 +3,24 @@
 angular.module('wdiSpa.search-results', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/search', {
+  $routeProvider.when('/search/:title?', {
     templateUrl: 'components/search-results/search.html',
     controller: 'SearchResultsCtrl'
   });
+  //$routeProvider.when('/search', {
+  //  templateUrl: 'components/search-results/search.html',
+  //  controller: 'SearchResultsCtrl'
+  //});
 }])
 
-.controller('SearchResultsCtrl', [function() {
+.controller('SearchResultsCtrl', ['HttpService', '$scope', '$routeParams', '$location', function(httpService, $scope, $routeParams, $location) {
 
+  if ($routeParams.title) {
+      httpService.search($routeParams.title)
+      .success(function (data) {
+        $scope.search = data.Search;
+        $scope.response = data.Error;
+            console.log(data);
+      });
+  }
 }]);
